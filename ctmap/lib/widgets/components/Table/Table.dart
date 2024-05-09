d5de2614a95c3fd4a4c098c1de597ded62db9338
thuong22092k3 <everything22092k3@gmail.com>
2024-05-09 11:38:07 +0700

    Fix - Table in Statistic

--- ctmap/lib/widgets/components/Table/Table.dart
+++ ctmap/lib/widgets/components/Table/Table.dart
@@ -10,32 +10,54 @@ class CustomTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+    return FutureBuilder<Map<String, int>>(
+      future: _getCityAccidentCount(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator(); 
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}'); 
+        } else {
+          Map<String, int> cityAccidentCount = snapshot.data!;
+        
+          List<String> sortedCities = cityAccidentCount.keys.toList()
+            ..sort((a, b) => cityAccidentCount[b]!.compareTo(cityAccidentCount[a]!));
+
+          return DataTable(
+            columns: [
+              DataColumn(label: Text('Thành phố')),
+              DataColumn(label: Text('SL')),
+            ],
+            rows: sortedCities
+                .map((city) => DataRow(cells: [
+                      DataCell(Text(city)),
+                      DataCell(Text(cityAccidentCount[city].toString())),
+                    ]))
+                .toList(),
+          );
+        }
+      },
+    );
+  }
+
+  Future<Map<String, int>> _getCityAccidentCount() async {
     Map<String, int> cityAccidentCount = {};
-    accidentDataList.forEach((accident) {
-      String city = accident.position.toString();
+
+    for (AccidentData accident in accidentDataList) {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          accident.position.latitude, accident.position.longitude);
+
+      String city = placemarks.isNotEmpty
+          ? placemarks[0].locality ?? 'Unknown'
+          : 'Unknown';
+
       if (cityAccidentCount.containsKey(city)) {
         cityAccidentCount[city] = cityAccidentCount[city]! + 1;
       } else {
         cityAccidentCount[city] = 1;
       }
-    });
+    }
 
-    // Sắp xếp danh sách các thành phố dựa trên số lượng tai nạn
-    List<String> sortedCities = cityAccidentCount.keys.toList()
-      ..sort((a, b) => cityAccidentCount[b]!.compareTo(cityAccidentCount[a]!));
-
-    return DataTable(
-      columns: [
-        DataColumn(label: Text('Thành phố')),
-        DataColumn(label: Text('SL')),
-      ],
-      rows: sortedCities
-          .map((city) => DataRow(cells: [
-                DataCell(Text(city)),
-                DataCell(Text(cityAccidentCount[city].toString())),
-              ]))
-          .toList(),
-    );
+    return cityAccidentCount;
   }
 }
