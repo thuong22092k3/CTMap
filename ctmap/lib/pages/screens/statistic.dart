77b0af428b9c1570a786efe34144fe484b4c35dc
thuong22092k3 <everything22092k3@gmail.com>
2024-06-16 07:39:09 +0700

    UPDATE - Statistic: Add Sum of Accidents

--- ctmap/lib/pages/screens/statistic.dart
+++ ctmap/lib/pages/screens/statistic.dart
@@ -110,108 +110,114 @@ class _StatisticState extends State<Statistic> {
                         btnWidth: 140,
                         fontSize: 14,
                         btnColor: isListButtonSelected
-                            ? AppColors.red // Màu đỏ khi được chọn
-                            : AppColors.primaryGray, // Màu mặc định
+                            ? AppColors.red
+                            : AppColors.primaryGray,
                       ),
                     ],
                   ),
                   SizedBox(height: 20),
-                  if (showChart)
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          CustomDropdown(
-                            items: cities,
-                            selectedItem: selectedCity,
-                            dropdownHeight: 30,
-                            onChanged: (newValue) {
-                              setState(() {
-                                selectedCity = newValue ?? selectedCity;
-                              });
-                            },
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              Expanded(
-                                flex: 1,
-                                child: CustomDropdown(
-                                  items: options,
-                                  selectedItem: selectedOption,
-                                  dropdownHeight: 30,
-                                  borderRadius: 0,
-                                  onChanged: (newValue) {
-                                    setState(() {
-                                      selectedOption =
-                                          newValue ?? selectedOption;
-                                    });
-                                  },
-                                ),
-                              ),
-                              VerticalDivider(color: AppColors.black),
-                              Expanded(
-                                flex: 1,
-                                child: CustomButton(
-                                  onTap: () {},
-                                  icon: AppIcons.calendar,
-                                  iconColor: AppColors.red,
-                                  btnHeight: 35,
-                                  btnColor: AppColors.lightGrey,
-                                ),
-                              )
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          CustomButton(
-                            onTap: () {},
-                            btnText: "Tìm kiếm",
-                            btnHeight: 30,
-                            borderRadius: 5,
-                            btnWidth: 140,
-                            fontSize: 14,
-                          ),
-                        ],
-                      ),
-                    ),
+                  Text(
+                    'Tổng số vụ tai nạn: ${accidentDataList.length}',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
                   SizedBox(height: 20),
                   if (showChart)
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 300,
-                            child: CustomBarChart(
-                              dataMode: DataMode.level,
-                              accidentDataList: accidentDataList,
+                    // Container(
+                    //   padding:
+                    //       EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+
+                    //   // decoration: BoxDecoration(
+                    //   //   color: Colors.white,
+                    //   //   borderRadius: BorderRadius.circular(10),
+                    //   //   boxShadow: [
+                    //   //     BoxShadow(
+                    //   //       color: Colors.grey.withOpacity(0.5),
+                    //   //       spreadRadius: 2,
+                    //   //       blurRadius: 5,
+                    //   //       offset: Offset(0, 3),
+                    //   //     ),
+                    //   //   ],
+                    //   // ),
+                    //   // child: Column(
+                    //   //   crossAxisAlignment: CrossAxisAlignment.stretch,
+                    //   //   children: [
+                    //   //     CustomDropdown(
+                    //   //       items: cities,
+                    //   //       selectedItem: selectedCity,
+                    //   //       dropdownHeight: 30,
+                    //   //       onChanged: (newValue) {
+                    //   //         setState(() {
+                    //   //           selectedCity = newValue ?? selectedCity;
+                    //   //         });
+                    //   //       },
+                    //   //     ),
+                    //   //     SizedBox(height: 10),
+                    //   //     Row(
+                    //   //       children: [
+                    //   //         Expanded(
+                    //   //           flex: 1,
+                    //   //           child: CustomDropdown(
+                    //   //             items: options,
+                    //   //             selectedItem: selectedOption,
+                    //   //             dropdownHeight: 30,
+                    //   //             borderRadius: 0,
+                    //   //             onChanged: (newValue) {
+                    //   //               setState(() {
+                    //   //                 selectedOption =
+                    //   //                     newValue ?? selectedOption;
+                    //   //               });
+                    //   //             },
+                    //   //           ),
+                    //   //         ),
+                    //   //         VerticalDivider(color: AppColors.black),
+                    //   //         Expanded(
+                    //   //           flex: 1,
+                    //   //           child: CustomButton(
+                    //   //             onTap: () {},
+                    //   //             icon: AppIcons.calendar,
+                    //   //             iconColor: AppColors.red,
+                    //   //             btnHeight: 35,
+                    //   //             btnColor: AppColors.lightGrey,
+                    //   //           ),
+                    //   //         )
+                    //   //       ],
+                    //   //     ),
+                    //   //     SizedBox(height: 10),
+                    //   //     CustomButton(
+                    //   //       onTap: () {},
+                    //   //       btnText: "Tìm kiếm",
+                    //   //       btnHeight: 30,
+                    //   //       borderRadius: 5,
+                    //   //       btnWidth: 140,
+                    //   //       fontSize: 14,
+                    //   //     ),
+                    //   //   ],
+                    //   // ),
+                    // ),
+                    // SizedBox(height: 20),
+                    if (showChart)
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 300,
+                              child: CustomBarChart(
+                                dataMode: DataMode.level,
+                                accidentDataList: accidentDataList,
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 20),
-                          SizedBox(
-                            height: 300,
-                            child: CustomBarChart(
-                              dataMode: DataMode.cause,
-                              accidentDataList: accidentDataList,
+                            SizedBox(height: 20),
+                            SizedBox(
+                              height: 300,
+                              child: CustomBarChart(
+                                dataMode: DataMode.cause,
+                                accidentDataList: accidentDataList,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
                   if (!showChart)
                     SizedBox(
                       child: CustomTable(accidentDataList: accidentDataList),
