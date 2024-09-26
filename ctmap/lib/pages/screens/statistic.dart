d7fd1c4d2d0caa815df481e50c4d982e076843d8
thuong22092k3 <everything22092k3@gmail.com>
2024-09-26 22:21:03 +0700

    ADD - Filter function by city

--- ctmap/lib/pages/screens/statistic.dart
+++ ctmap/lib/pages/screens/statistic.dart
@@ -16,7 +16,7 @@ class Statistic extends StatefulWidget {
 }
 
 class _StatisticState extends State<Statistic> {
-  List<String> cities = ['Tất cả', 'TP Hồ Chí Minh', 'Gia Lai', 'Bình Dương'];
+  List<String> cities = ['Tất cả'];
   String selectedCity = 'Tất cả';
   List<String> options = ['Ngày', 'Tuần', 'Tháng'];
   String selectedOption = 'Ngày';
@@ -24,6 +24,10 @@ class _StatisticState extends State<Statistic> {
   bool isListButtonSelected = false;
   bool isChartButtonSelected = true;
   List<AccidentData> accidentDataList = [];
+  List<AccidentData> filteredAccidentDataList = [];
+  Map<String, int> accidentCountByCity = {};
+
+  @override
   void initState() {
     super.initState();
     getAccidents();
@@ -33,18 +37,47 @@ class _StatisticState extends State<Statistic> {
     List<AccidentData> accidents = await getAllAccidents();
     if (accidents.isNotEmpty) {
       print('Dữ liệu đã được lấy thành công.');
+
+      Set<String> uniqueCities =
+          accidents.map((accident) => accident.city).toSet();
+
+      setState(() {
+        accidentDataList = accidents;
+        filteredAccidentDataList = accidents;
+        cities = ['Tất cả', ...uniqueCities];
+        accidentCountByCity = _countAccidentsByCity(accidents);
+      });
     } else {
       print('Không có dữ liệu.');
     }
+  }
+
+  Map<String, int> _countAccidentsByCity(List<AccidentData> accidents) {
+    Map<String, int> cityAccidentCount = {};
+    for (var accident in accidents) {
+      cityAccidentCount[accident.city] =
+          (cityAccidentCount[accident.city] ?? 0) + 1;
+    }
+    return cityAccidentCount;
+  }
+
+  void filterAccidents() {
     setState(() {
-      accidentDataList = accidents;
+      if (selectedCity == 'Tất cả') {
+        filteredAccidentDataList = accidentDataList;
+      } else {
+        filteredAccidentDataList = accidentDataList
+            .where((accident) => accident.city == selectedCity)
+            .toList();
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // final accidentDataList = context.read(accidentDataProvider).state;
-    print('Accident Data List in Statistic: $accidentDataList');
+    print(
+        'Filtered Accident Data List in Statistic: $filteredAccidentDataList');
+
     return Scaffold(
       body: CustomScrollView(
         slivers: [
@@ -117,110 +150,112 @@ class _StatisticState extends State<Statistic> {
                   ),
                   SizedBox(height: 20),
                   Text(
-                    'Tổng số vụ tai nạn: ${accidentDataList.length}',
+                    'Tổng số vụ tai nạn: ${filteredAccidentDataList.length}',
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
                   ),
                   SizedBox(height: 20),
                   if (showChart)
-                    // Container(
-                    //   padding:
-                    //       EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-
-                    //   // decoration: BoxDecoration(
-                    //   //   color: Colors.white,
-                    //   //   borderRadius: BorderRadius.circular(10),
-                    //   //   boxShadow: [
-                    //   //     BoxShadow(
-                    //   //       color: Colors.grey.withOpacity(0.5),
-                    //   //       spreadRadius: 2,
-                    //   //       blurRadius: 5,
-                    //   //       offset: Offset(0, 3),
-                    //   //     ),
-                    //   //   ],
-                    //   // ),
-                    //   // child: Column(
-                    //   //   crossAxisAlignment: CrossAxisAlignment.stretch,
-                    //   //   children: [
-                    //   //     CustomDropdown(
-                    //   //       items: cities,
-                    //   //       selectedItem: selectedCity,
-                    //   //       dropdownHeight: 30,
-                    //   //       onChanged: (newValue) {
-                    //   //         setState(() {
-                    //   //           selectedCity = newValue ?? selectedCity;
-                    //   //         });
-                    //   //       },
-                    //   //     ),
-                    //   //     SizedBox(height: 10),
-                    //   //     Row(
-                    //   //       children: [
-                    //   //         Expanded(
-                    //   //           flex: 1,
-                    //   //           child: CustomDropdown(
-                    //   //             items: options,
-                    //   //             selectedItem: selectedOption,
-                    //   //             dropdownHeight: 30,
-                    //   //             borderRadius: 0,
-                    //   //             onChanged: (newValue) {
-                    //   //               setState(() {
-                    //   //                 selectedOption =
-                    //   //                     newValue ?? selectedOption;
-                    //   //               });
-                    //   //             },
-                    //   //           ),
-                    //   //         ),
-                    //   //         VerticalDivider(color: AppColors.black),
-                    //   //         Expanded(
-                    //   //           flex: 1,
-                    //   //           child: CustomButton(
-                    //   //             onTap: () {},
-                    //   //             icon: AppIcons.calendar,
-                    //   //             iconColor: AppColors.red,
-                    //   //             btnHeight: 35,
-                    //   //             btnColor: AppColors.lightGrey,
-                    //   //           ),
-                    //   //         )
-                    //   //       ],
-                    //   //     ),
-                    //   //     SizedBox(height: 10),
-                    //   //     CustomButton(
-                    //   //       onTap: () {},
-                    //   //       btnText: "Tìm kiếm",
-                    //   //       btnHeight: 30,
-                    //   //       borderRadius: 5,
-                    //   //       btnWidth: 140,
-                    //   //       fontSize: 14,
-                    //   //     ),
-                    //   //   ],
-                    //   // ),
-                    // ),
-                    // SizedBox(height: 20),
-                    if (showChart)
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 300,
-                              child: CustomBarChart(
-                                dataMode: DataMode.level,
-                                accidentDataList: accidentDataList,
-                              ),
-                            ),
-                            SizedBox(height: 20),
-                            SizedBox(
-                              height: 300,
-                              child: CustomBarChart(
-                                dataMode: DataMode.cause,
-                                accidentDataList: accidentDataList,
-                              ),
-                            ),
-                          ],
-                        ),
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
                       ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          CustomDropdown(
+                            items: cities,
+                            selectedItem: selectedCity,
+                            dropdownHeight: 30,
+                            onChanged: (newValue) {
+                              setState(() {
+                                selectedCity = newValue ?? selectedCity;
+                              });
+                            },
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: CustomDropdown(
+                                  items: options,
+                                  selectedItem: selectedOption,
+                                  dropdownHeight: 30,
+                                  borderRadius: 0,
+                                  onChanged: (newValue) {
+                                    setState(() {
+                                      selectedOption =
+                                          newValue ?? selectedOption;
+                                    });
+                                  },
+                                ),
+                              ),
+                              VerticalDivider(color: AppColors.black),
+                              Expanded(
+                                flex: 1,
+                                child: CustomButton(
+                                  onTap: () {},
+                                  icon: AppIcons.calendar,
+                                  iconColor: AppColors.red,
+                                  btnHeight: 35,
+                                  btnColor: AppColors.lightGrey,
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          CustomButton(
+                            onTap: () {
+                              filterAccidents();
+                            },
+                            btnText: "Tìm kiếm",
+                            btnHeight: 30,
+                            borderRadius: 5,
+                            btnWidth: 140,
+                            fontSize: 14,
+                          ),
+                        ],
+                      ),
+                    ),
+                  SizedBox(height: 20),
+                  if (showChart)
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 300,
+                            child: CustomBarChart(
+                              dataMode: DataMode.level,
+                              accidentDataList: filteredAccidentDataList,
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          SizedBox(
+                            height: 300,
+                            child: CustomBarChart(
+                              dataMode: DataMode.cause,
+                              accidentDataList: filteredAccidentDataList,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   if (!showChart)
                     SizedBox(
-                      child: CustomTable(accidentDataList: accidentDataList),
+                      child: CustomTable(
+                          accidentDataList: filteredAccidentDataList),
                     ),
                 ],
               ),
